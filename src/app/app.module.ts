a6e10a89aaa598192c54e36d15b96d1a2e6c4ea7
Giuliano Agustin Butera Arcari <giuliano.butera@tipsalud.local>
2019-01-22 09:49:15 -0300

    Segundo Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,28 +11,12 @@ import { PagesModule } from './pages/pages.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './login/register.component';
-// import { DashboardComponent } from './pages/dashboard/dashboard.component';
-// import { ProgressComponent } from './pages/progress/progress.component';
-// import { Graficas1Component } from './pages/graficas1/graficas1.component';
-// import { PagesComponent } from './pages/pages.component';
-// import { HeaderComponent } from './shared/header/header.component';
-// import { SidebarComponent } from './shared/sidebar/sidebar.component';
-// import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
-// import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     RegisterComponent,
-    // DashboardComponent,
-    // ProgressComponent,
-    // Graficas1Component,
-    // PagesComponent,
-    // HeaderComponent,
-    // SidebarComponent,
-    // BreadcrumbsComponent,
-    // NopagefoundComponent
   ],
   imports: [
     BrowserModule,
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -9,6 +9,9 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
 import { Graficas1Component } from './graficas1/graficas1.component';
 
+// Rutas
+import { PAGES_ROUTES } from './pages.routes';
+
 @NgModule({
     declarations: [
         PagesComponent,
@@ -17,7 +20,8 @@ import { Graficas1Component } from './graficas1/graficas1.component';
         Graficas1Component
     ],
     imports: [
-        SharedModule
+        SharedModule,
+        PAGES_ROUTES
     ],
     exports: [
         PagesComponent,
