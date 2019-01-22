b806d260fbaf0ce8a7c44a156a2ab52571d4e0df
Giuliano Agustin Butera Arcari <giuliano.butera@tipsalud.local>
2019-01-22 09:12:35 -0300

    Segundo Commit

---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+
+// Modulos
+import { SharedModule } from '../shared/shared.module';
+
+// Componentes
+import { PagesComponent } from './pages.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProgressComponent } from './progress/progress.component';
+import { Graficas1Component } from './graficas1/graficas1.component';
+
+@NgModule({
+    declarations: [
+        PagesComponent,
+        DashboardComponent,
+        ProgressComponent,
+        Graficas1Component
+    ],
+    imports: [
+        SharedModule
+    ],
+    exports: [
+        PagesComponent,
+        DashboardComponent,
+        ProgressComponent,
+        Graficas1Component
+    ]
+})
+
+export class PagesModule {}
